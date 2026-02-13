f21533dab6c19c5aaf1a521f891d19a9a592e3e9
hixam001 <itshishamahmed@gmail.com>
2026-02-13 16:47:43 +0500

    Completed 11_tempConversion Applied formulae to convert to celcius and fahrenheit Used math.round(num *10)/10 formula to convert to 1 decimal in case of multiple decimals

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celcius=Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const fahrenheit=Math.round(((celcius * 9 / 5) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
