4c0b87eac785abe85c9ccbb860da71bed041fa0e
Hisham Ahmed <itshishamahmed@gmail.com>
2026-03-05 16:24:51 +0500

    Completed 13_palindromes Created a string to have all the alphanumerical characters Created another string to have all the valid characters from the string input and also cleaned it up Then reversed that string and compared the both Returned the answer to the comparision

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const char1='abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const string1=string.toLowerCase().split('').filter((character) => char1.includes(character)).join('');
+
+    const reversed=string1.split('').reverse().join('');
+
+    return string1===reversed;
 };
 
 // Do not edit below this line
