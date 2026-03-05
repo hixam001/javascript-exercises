9001d1d1256a9a423d7aa7c5716d13766239e78f
Hisham Ahmed <itshishamahmed@gmail.com>
2026-03-05 16:10:23 +0500

    Completed 12_calculator Appended all the functions as per the instruction in the readme Tested all the cases in spec file

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum=0
+	for(let i=0; i<array.length;i++){
+    sum+=array[i];
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mul=1;
+  for(let i=0;i<array.length;i++){
+    mul*=array[i];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let fact=1
+  for(let i=1;i<=num1;i++){
+    fact*=i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
