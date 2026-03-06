7160ceefaa44017eaadb5306fa8083c4556dbab2
Hisham Ahmed <itshishamahmed@gmail.com>
2026-03-06 12:41:47 +0500

    Completed 14_fibonacci parsed the function parameter into int Put conditions to check if the parameter is less than 0 Put conditions to check if the parameter is equal to 0 Put condition to check if the parameter is in the range of 1-2 Applied a for loop to iterate and calculate the fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    count=parseInt(num);
+    let a=1
+    let b=0
+    if(count<0){
+        return 'OOPS'
+    }else if(count ===0){
+        return 0
+    }else if(count>0 && count<3){
+        return a;
+    }else{
+        for(let i=2;i<=count;i++){
+            let current=a+b;
+            b=a;
+            a=current;
+        }
+        return a
+    }
 };
 
 // Do not edit below this line
