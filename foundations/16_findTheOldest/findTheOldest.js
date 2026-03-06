fce47146c9ce0adca5a5d79103fafb7d888c9f9b
Hisham Ahmed <itshishamahmed@gmail.com>
2026-03-06 13:09:51 +0500

    Completed 16_findTheOldest Created a new function to determine the Age Used that new function to determine age in the main function and compare Stored the oldest on in the oldest variable Returned that variable

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+function getAge(birth,death){
+    if(!death){
+        death= new Date().getFullYear();
+    }
+    return death-birth;
+}
 
+const findTheOldest = function(array) {
+    let oldest;
+    oldest=array[0]
+    for(let i=0;i<array.length;i++){
+        if(getAge(oldest.yearOfBirth,oldest.yearOfDeath)<getAge(array[i]['yearOfBirth'],array[i]['yearOfDeath'])){
+            oldest=array[i]
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
