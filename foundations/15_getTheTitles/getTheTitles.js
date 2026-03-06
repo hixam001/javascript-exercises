b2a45bfa0dec75074f39be89841bf9af5d7201e4
Hisham Ahmed <itshishamahmed@gmail.com>
2026-03-06 12:48:00 +0500

    Completed 15_getTheTitles Looped through the array given in the parameters Used double indexing to get the title and strored it in another array

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const bookTitle=[];
+    for(let i=0;i<array.length;i++){
+        bookTitle[i]=array[i]['title'];
+    }
+    return bookTitle;
 };
 
 // Do not edit below this line
